1e8f79fa419809d9e67087fea1124a56b389bc24
Manisha <Manisha@ramesh001>
2026-02-12 22:23:16 +0530

    updatec operator controller on 20260212 10:22pm

--- src/main/java/com/properties/controller/operatorcontroller.java
+++ src/main/java/com/properties/controller/operatorcontroller.java
@@ -3,9 +3,10 @@
  */
 package com.properties.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.properties.component.celcomprop;
 import com.properties.component.maxissysprop;
@@ -16,6 +17,7 @@ import com.properties.component.maxissysprop;
 @RestController
 //@RequestMapping("/getproperty")
 public class operatorcontroller {
+	private static final Logger logger = LogManager.getLogger(operatorcontroller.class);
 
 	  @Autowired
 	  private maxissysprop maxissysprop;
@@ -28,7 +30,9 @@ public class operatorcontroller {
 	@GetMapping("/getLip4maxis")
 	public String getLip4maxis() {
 		
-		System.out.println("Maxis IP  ----> : "+ maxissysprop.getLip4maxis());
+		info("Maxis IP  ----> : "+ maxissysprop.getLip4maxis());
+		
+		info("Maxis IP changed in source  ----> : "+ maxissysprop.getLip4telco());
 		
 		return maxissysprop.getLip4maxis();
 	} // End of getLip4maxis
@@ -41,5 +45,5 @@ public class operatorcontroller {
 		
 	} // End of getLib4celcom
 	
-	
+	private void info(String strPrint) {logger.info(strPrint);}
 } // End of propertycontroller
